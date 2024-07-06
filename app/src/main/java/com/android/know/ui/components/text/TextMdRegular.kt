f0d14a0220a81128227feecf2a308f186d50d06c
Kalina Valeva <>
2024-07-06 11:04:46 +0300

    - adding text styles

---
+++ app/src/main/java/com/android/know/ui/components/text/TextMdRegular.kt
@@ -0,0 +1,26 @@
+package com.android.know.ui.components.text
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.android.know.R
+
+@Composable
+fun TextMdRegular(
+    text: String,
+    color: Color = colorResource(id = R.color.black).copy(0.4f),
+) {
+    Text(
+        text = text,
+        color = color,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        fontFamily = FontFamily(Font(R.font.inter)),
+        fontWeight = FontWeight(400)
+    )
+}
---
+++ app/src/main/java/com/android/know/ui/components/text/TextXsMedium.kt
@@ -0,0 +1,26 @@
+package com.android.know.ui.components.text
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.android.know.R
+
+@Composable
+fun TextXsMedium(
+    text: String,
+    color: Color = colorResource(id = R.color.content_tertiary),
+) {
+    Text(
+        text = text,
+        fontSize = 12.sp,
+        lineHeight = 18.sp,
+        fontFamily = FontFamily(Font(R.font.inter)),
+        fontWeight = FontWeight(500),
+        color = color
+    )
+}
